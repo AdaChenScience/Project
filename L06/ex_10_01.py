1b3187c605c7d4fbe14db4ae28fea78bd3a30539
Ada Chen <151989052+AdaChenScience@users.noreply.github.com>
2024-03-12 21:06:02 +0800

    Delete L06 directory

--- L06/ex_10_01.py
+++
@@ -1,3 +0,0 @@
-import numpy as np
-sample=np.random.normal(0,1,size=100)
-print(sample)
--- L06/ex_10_04.py
+++
@@ -1,9 +0,0 @@
-import numpy as np
-
-A = np.array([[2, 1], [4, 5]])
-x = np.ones(A.shape[0])
-for k in range(50): # 幂迭代，迭代次数为50
-    x = A @ x
-x = x / np.linalg.norm(x)
-lambda1 = A @ x @ x / (x @ x) # 瑞利商
-print('最大特征值为:',lambda1)
