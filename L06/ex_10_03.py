6101784ade26e98464e2df038f66c07069b7b8ec
Ada Chen <151989052+AdaChenScience@users.noreply.github.com>
2024-03-12 21:54:48 +0800

    Delete L06 directory

--- L06/ex_10_03.py
+++
@@ -1,7 +0,0 @@
-import numpy as np
-
-mat = np.array([[2, 1], [4, 5]])
-eigenvalue, featurevector = np.linalg.eig(mat)
-
-print("该矩阵特征值：\n", eigenvalue)
-print("特征向量（对应列向量）：\n", featurevector)
--- L06/ex_10_05.py
+++
@@ -1,5 +0,0 @@
-import numpy as np
-
-mat = np.array([[1,-1,4], [2,1,3], [1,3,-1]]).transpose() #数据为三个行向量
-C = np.cov(mat)
-print(C)
